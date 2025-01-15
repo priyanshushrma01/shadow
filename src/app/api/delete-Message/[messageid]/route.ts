665fa96bfa3c9820224c2d3b19e0878809c1169b
Priyanshu Sharma <priyanshu.sh18@gmail.com>
2025-01-15 23:08:49 +0530

    fix deployment

--- src/app/api/delete-Message/[messageid]/route.ts
+++ src/app/api/delete-Message/[messageid]/route.ts
@@ -4,16 +4,20 @@ import dbConnect from '@/lib/dbConnect';
 import { User } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/options';
 
+import { NextRequest, NextResponse } from 'next/server';
+
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { messageid: string } }
 ) {
   const messageId = params.messageid;
   await dbConnect();
+
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
+
   if (!session || !user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
@@ -26,19 +30,19 @@ export async function DELETE(
     );
 
     if (updateResult.modifiedCount === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: 'Message not found or already deleted', success: false },
         { status: 404 }
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { message: 'Message deleted', success: true },
       { status: 200 }
     );
   } catch (error) {
     console.error('Error deleting message:', error);
-    return Response.json(
+    return NextResponse.json(
       { message: 'Error deleting message', success: false },
       { status: 500 }
     );
